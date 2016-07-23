8571936f21547174199229cc9e597ef9849c553b
Naz <admin@admins-MacBook-Pro.local>
2016-07-23 07:59:51 +0100

    Completed initial UI design

--- ViewController.designer.cs
+++ ViewController.designer.cs
@@ -1,14 +1,92 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
 
-namespace TipCalculator {
-	[Register("ViewController")]
-	partial class ViewController {
-		void ReleaseDesignerOutlets() {
-		}
-	}
+namespace TipCalculator
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel labelTipPercentage { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel labelTipTotalValue { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel labelTotalBillValue { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel labelTotalPerPersonValue { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider sliderTipPercentage { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIStepper stepperNumberOfPeople { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField textFieldBillTotal { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField textFieldNumberOfPeople { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (labelTipPercentage != null) {
+                labelTipPercentage.Dispose ();
+                labelTipPercentage = null;
+            }
+
+            if (labelTipTotalValue != null) {
+                labelTipTotalValue.Dispose ();
+                labelTipTotalValue = null;
+            }
+
+            if (labelTotalBillValue != null) {
+                labelTotalBillValue.Dispose ();
+                labelTotalBillValue = null;
+            }
+
+            if (labelTotalPerPersonValue != null) {
+                labelTotalPerPersonValue.Dispose ();
+                labelTotalPerPersonValue = null;
+            }
+
+            if (sliderTipPercentage != null) {
+                sliderTipPercentage.Dispose ();
+                sliderTipPercentage = null;
+            }
+
+            if (stepperNumberOfPeople != null) {
+                stepperNumberOfPeople.Dispose ();
+                stepperNumberOfPeople = null;
+            }
+
+            if (textFieldBillTotal != null) {
+                textFieldBillTotal.Dispose ();
+                textFieldBillTotal = null;
+            }
+
+            if (textFieldNumberOfPeople != null) {
+                textFieldNumberOfPeople.Dispose ();
+                textFieldNumberOfPeople = null;
+            }
+        }
+    }
 }
